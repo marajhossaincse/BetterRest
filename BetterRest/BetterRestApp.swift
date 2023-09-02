3b9d0df0c084a75a2b9dc59e45fe68a7716991a8
Maraj Hossain <marajhossaincse@gmail.com>
2023-09-02 23:24:13 +0600

    initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Maraj Hossain on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
