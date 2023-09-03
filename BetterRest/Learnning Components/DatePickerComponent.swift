ee99cd474c4cd765b19b5c9090b44dbfbcef10b1
Maraj Hossain <marajhossaincse@gmail.com>
2023-09-03 16:53:47 +0600

    learn how to work with dates

--- BetterRest/Learnning Components/DatePickerComponent.swift
+++ BetterRest/Learnning Components/DatePickerComponent.swift
@@ -11,12 +11,40 @@ struct DatePickerComponent: View {
     @State private var wakeUp = Date.now
 
     var body: some View {
-        DatePicker(
-            "Select a date",
-            selection: $wakeUp,
-            displayedComponents: .date
-        )
+        VStack(alignment: .leading, spacing: 20) {
+            DatePicker(
+                "Select a date",
+                selection: $wakeUp,
+                in: Date.now...,
+                displayedComponents: .date
+            )
 //            .labelsHidden()
+
+            Text(Date.now, format: .dateTime.day().month().year())
+
+            Text(Date.now, format: .dateTime.hour().minute())
+
+            Text(Date.now.formatted(date: .long, time: .shortened))
+        }
+        .padding()
+    }
+
+    func trivialExample() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//
+//        let date = Calendar.current.date(from: components) ?? Date.now
+    }
+
+    func exampleDate() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now ... tomorrow
     }
 }
 
